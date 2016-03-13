9395d07830d594bd5534315377a1bab85deefad9
Jason Hee <jasonheecs@gmail.com>
2016-03-14 02:53:20 +0800

    Added Shape and Dot objects to shape-shifter

--- app/_js/shape-shifter.js
+++ app/_js/shape-shifter.js
@@ -78,6 +78,123 @@ Color.prototype.render = function() {
 	return 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.a + ')';
 };
 
+var Dot = function(x, y) {
+	this.Point = new Point({
+		x: x,
+		y: y,
+		z: 5,
+		a: 1,
+		h: 0
+	});
+
+	//TODO
+	this.e = 0.07;
+	this.s = true;
+
+	this.Color = new Color(31, 31, 31, this.Point.a);
+
+	this.t = this.clone();
+	this.q = [];
+};
+
+Dot.prototype = {
+	clone: function() {
+		return new Point({
+			x: this.x,
+			y: this.y,
+			z: this.z,
+			a: this.a,
+			h: this.h
+		});
+	},
+
+	_draw: function() {
+		this.Color.a = this.Point.a;
+		Drawing.drawCircle(this.Point, this.Color);
+	},
+
+	_moveTowards: function(Point2) {
+		var details = this.distanceTo(Point2, true);
+		var deltaX = details[0];
+		var deltaY = details[1];
+		var distance = details[2];
+		var e = this.e * distance;
+
+		if (this.Point.h === -1) {
+			this.Point.x = Point2.x;
+			this.Point.y = Point2.y;
+			return true;
+		}
+
+		if (distance > 1) {
+			this.Point.x -= ((deltaX / distance) * e);
+			this.Point.y -= ((deltaY / distance) * e);
+		} else {
+			if (this.Point.h > 0) {
+				this.Point.h--;
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	},
+
+	distanceTo: function(n, details) { //calculate the distance between 2 Points using Pythagorean theorem
+		var dx = this.Point.x - n.x;
+		var dy = this.Point.y - n.y;
+		var d = Math.sqrt(dx * dx + dy * dy);
+
+		return details ? [dx, dy, d] : d;
+	},
+
+	_update: function() {
+		var pt;
+		var delta;
+
+		if (this._moveTowards(this.t)) {
+			pt = this.q.shift();
+
+			if (pt) {
+				this.t.x = pt.x || this.Point.x;
+				this.t.y = pt.y || this.Point.y;
+				this.t.z = pt.z || this.Point.z;
+				this.t.a = pt.a || this.Point.a;
+				this.Point.h = pt.h || 0;
+			} else {
+				if (this.s) {
+					this.Point.x -= Math.sin(Math.random() * Math.PI);
+					this.Point.y -= Math.sin(Math.random() * Math.PI);
+				} else {
+					this.move(new Point({
+						x: this.Point.x + (Math.random() * 50) - 25,
+						y: this.Point.y + (Math.random() * 50) - 25	
+					}));
+				}
+			}
+		}
+
+		//alpha
+		delta = this.Point.a - this.t.a;
+		this.Point.a = Math.max(0.1, this.Point.a - (delta * 0.05));
+		//z
+		delta = this.Point.z - this.t.z;
+		this.Point.z = Math.max(1, this.Point.z - (delta * 0.05));
+	},
+
+	move: function(pt, avoidStatic) {
+		if (!avoidStatic || (avoidStatic && this.distanceTo(pt) > 1)) {
+			this.q.push(pt);
+		}
+	},
+
+	render: function() {
+		this._update();
+		this._draw();
+	}
+};
+
+
 module.exports = {
 	init: function() {
 		Drawing.init('.canvas');
