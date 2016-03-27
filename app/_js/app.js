85095d5c892bca1ba76854c50aa6c9e0cedf286c
Jason Hee <jasonheecs@gmail.com>
2016-03-28 00:13:59 +0800

    Added Smoothstate

--- app/_js/app.js
+++ app/_js/app.js
@@ -3,6 +3,7 @@
 var ShapeShifter = require('./shape-shifter');
 var ViewportAnimator = require('./viewport-animator');
 var Nav = require('./nav');
+var Smoothstate = require('./smoothstate');
 
 document.addEventListener('DOMContentLoaded', function() {
 	if (document.querySelector('.canvas')) {
@@ -11,4 +12,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	Nav.init();
 	ViewportAnimator.init();
+	Smoothstate.init();
 });
---
+++ app/_js/smoothstate.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var $ = require('jquery');
+var smoothState = require('smoothstate');
+
+function init() {
+	console.log('init smoothstate');
+}
+
+module.exports = {
+	init: init
+};
